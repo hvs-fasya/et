6005efe3ca8da1f180a3070113dfb642212f64cf
Elena Tolkacheva <hvs-fasya@mail.ru>
2017-06-08 16:18:47 +0300

    bootstrap super design

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,47 @@ class UsersController < ApplicationController
 		@users = fake_users
 	end
 
+	def new
+	  	@user = OpenStruct.new(:fullname => '', :email => '', :contacts => '')
+	end
+
+	def create
+		@user = OpenStruct.new(params[:user])
+	    if !@user
+	      redirect_to user_path(@user)
+	    else
+	      flash[:danger] = "Smth went wrong (((" 
+	      redirect_to edit_user_path(@user)
+	    end
+	end
+
+	def show
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
 	private
 
 	def fake_users
 		users = Array.new()
-		users.push(OpenStruct.new(:fullname => 'fullname1', :email => 'email1', :contacts => 'contacts1'))
-		users.push(OpenStruct.new(:fullname => 'fullname2', :email => 'email2', :contacts => 'contacts2'))
-		users.push(OpenStruct.new(:fullname => 'fullname3', :email => 'email3', :contacts => 'contacts3'))
+		users.push(OpenStruct.new(
+			:fullname => 'Иванов Иван Иванович',
+			:email => Array.new() << 'ivanov@example.com' << 'ivanov@ivanov.com',
+			:contacts => 'contacts1'))
+		users.push(OpenStruct.new(
+			:fullname => 'Сидоров Сидор Сидорович',
+			:email => Array.new() << 'sidorov@example.com',
+			:contacts => 'contacts2'))
+		users.push(OpenStruct.new(
+			:fullname => 'Эйабдурахман-оглы Теймураз Хафиздурмухаммедович',
+			:email => Array.new() << 'ogly@example.com',
+			:contacts => 'contacts3'))
 		users
 	end
 	
